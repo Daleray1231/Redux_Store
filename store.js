b9dd532bc23fc5c95fa87bc12f0cabdab179c43c
Daleray1231 <daleray1231@gmail.com>
2024-01-30 12:22:56 -0600

    updated store.js/remove asset folder

--- store.js
+++ store.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux'
-import rootReducer from './reducer'
+// store.js
+import { createStore } from 'redux';
+import rootReducer from './reducers'; // Assuming you have a rootReducer file
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer);
 
-export default store
+export default store;
